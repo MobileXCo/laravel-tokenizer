27e5083a892bb3c10d0f45502d62c2bb84f0ffd9
Patrique Ouimet <patrique@mobilexco.com>
2019-07-13 19:18:50 -0400

    Added comments to config

--- config/tokenizer.php
+++ config/tokenizer.php
@@ -1,7 +1,14 @@
 <?php
 
 return [
+    /**
+     * Token delimiter ':' to use for wrapping token keys
+     * Example :my_token:
+     */
     'token_delimiter' => ':',
 
+    /**
+     * Determins if string replacements should be case sensitive
+     */
     'is_case_sensitive' => true,
 ];
